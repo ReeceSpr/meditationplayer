f722698c46c056a178de511708c2750aa27cd979
ReeceSpr <reece.spragg@gmail.com>
2021-05-31 10:04:40 +1200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div id="Player">
       <div id="Controls">
-        <div id="Song Details">
+        <div id="LeftButtons">
+          <button id="Search" />
+          <button id="Upload" />
+          <button id="Website" />
+          <button id="PreviousSong" />
+          <button id="NextSong" />
+        </div>
+
+        <div id="VolumeButtons">
+          <button id="VolUp" />
+          <button id="VolDown" />
+        </div>
+
+        <div id="SongDetails">
           <h1>Song Name</h1>
           <p>Artist</p>
         </div>
-        <div id="LeftButtons">
 
-        </div>
-        <div id="VolumeButtons">
-
-        </div>
-        <div id="Rating">
-
-        </div>
+        <div id="Rating" />
       </div>
     </div>
   );
